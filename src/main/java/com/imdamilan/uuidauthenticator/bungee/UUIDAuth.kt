76c96f87d6894b30c6460e0a4b7313479193eec5
ImDaMilan <milanmiljkovic007@gmail.com>
2022-08-02 16:32:48 +0200

    Add Bungee autoupdate

--- src/main/java/com/imdamilan/uuidauthenticator/bungee/UUIDAuth.kt
+++ src/main/java/com/imdamilan/uuidauthenticator/bungee/UUIDAuth.kt
@@ -4,6 +4,7 @@ import com.imdamilan.uuidauthenticator.bungee.config.Config
 import com.imdamilan.uuidauthenticator.bungee.fileauth.FileAuth
 import com.imdamilan.uuidauthenticator.bungee.listeners.PlayerJoinListener
 import com.imdamilan.uuidauthenticator.bungee.sql.SQL
+import com.imdamilan.uuidauthenticator.bungee.update.Update
 import net.md_5.bungee.api.plugin.Plugin
 import net.md_5.bungee.config.Configuration
 import org.bstats.bungeecord.Metrics
@@ -29,9 +30,20 @@ class UUIDAuth : Plugin() {
         } else if (config.getBoolean("file-auth.enabled")) {
             FileAuth.initAuthFile()
         }
+        update()
     }
 
     override fun onDisable() {
         logger.info("UUIDAuth is disabled!")
+    }
+
+    private fun update() {
+        if (!Update.isLatest(102870)) {
+            if (config.getBoolean("autoupdate-enabled")) {
+                Update.updatePlugin(102870)
+            } else {
+                logger.severe("You are using an outdated version of UUIDAuth. Please update to the latest version!")
+            }
+        }
     }
 }
